2e69aa24555ba8ef92ffefd07270b8fb87de4501
masahiro nishiba <nishiba.m@gmail.com>
2018-12-26 14:30:03 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+
+readme_note = """\
+.. note::
+
+   For the latest source, discussion, etc, please visit the
+   `GitHub repository <https://github.com/m3dev/redshells>`_\n\n
+"""
+
+with open('README.md') as f:
+    long_description = readme_note + f.read()
+
+install_requires = [
+    'luigi',
+    'gokart',
+    'pandas',
+    'numpy',
+    'gensim',
+    'sklearn',
+]
+
+setup(
+    name='redshells',
+    version='0.1.0',
+    description=
+    'Tasks which are defined using gokart.TaskOnKart. The tasks can be used with data pipeline library "luigi".',
+    long_description=long_description,
+    author='M3, inc.',
+    url='https://github.com/m3dev/redshells',
+    license='MIT License',
+    packages=['redshells'],
+    install_requires=install_requires,
+    classifiers=[
+        'Programming Language :: Python :: 3.7',
+    ],
+)
