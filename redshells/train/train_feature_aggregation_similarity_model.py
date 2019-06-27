a336870ee65c70b880a46a8936ae170550e9b71b
masahiro nishiba <nishiba.m@gmail.com>
2019-06-27 11:04:30 +0900

    implement feature aggregation similarity model

---
+++ redshells/train/train_feature_aggregation_similarity_model.py
@@ -0,0 +1,56 @@
+from logging import getLogger
+
+import luigi
+import numpy as np
+
+import gokart
+
+from redshells.model import FeatureAggregationSimilarityModel
+from redshells.model.feature_aggregation_similarity_model import FeatureAggregationSimilarityDataset
+
+logger = getLogger(__name__)
+
+
+class TrainFeatureAggregationSimilarityModel(gokart.TaskOnKart):
+    dataset_task = gokart.TaskInstanceParameter(description='An instance of task which outputs `FeatureAggregationSimilarityDataset`.')
+    embedding_size = luigi.IntParameter()  # type: int
+    learning_rate = luigi.FloatParameter()  # type: float
+    batch_size = luigi.IntParameter()  # type: int
+    epoch_size = luigi.IntParameter()  # type: int
+    test_size_rate = luigi.FloatParameter()  # type: float
+    early_stopping_patience = luigi.IntParameter()  # type: int
+    max_data_size = luigi.IntParameter()  # type: int
+
+    def requires(self):
+        return self.dataset_task
+
+    def output(self):
+        return self.make_target('star_space_similarity/model.pkl')
+
+    def run(self):
+        dataset = self.load()  # type: FeatureAggregationSimilarityDataset
+        feature_size = dataset.x_item_features.shape[1]
+        item_size = max(np.max(dataset.x_item_indices), np.max(dataset.y_item_indices))
+        max_feature_index = max(np.max(dataset.x_item_features), np.max(dataset.y_item_features))
+
+        logger.debug(f'embedding_size={self.embedding_size},'
+                     f'learning_rate={self.learning_rate},'
+                     f'feature_size={feature_size},'
+                     f'item_size={item_size},'
+                     f'max_feature_index={max_feature_index}')
+
+        model = FeatureAggregationSimilarityModel(
+            embedding_size=self.embedding_size,
+            learning_rate=self.learning_rate,
+            feature_size=feature_size,
+            item_size=item_size,
+            max_feature_index=max_feature_index)
+
+        model.fit(
+            dataset=dataset.get(size=self.max_data_size),
+            batch_size=self.batch_size,
+            epoch_size=self.epoch_size,
+            test_size_rate=self.test_size_rate,
+            early_stopping_patience=self.early_stopping_patience)
+
+        self.dump(model)
---
+++ test/model/test_feature_aggregation_similarity_model.py
@@ -0,0 +1,37 @@
+import unittest
+from logging import getLogger
+
+import numpy as np
+
+from redshells.model import FeatureAggregationSimilarityModel
+from redshells.model.feature_aggregation_similarity_model import FeatureAggregationSimilarityDataset
+
+logger = getLogger(__name__)
+
+
+class TestFeatureAggregationSimilarityModel(unittest.TestCase):
+    def test(self):
+        model = FeatureAggregationSimilarityModel(embedding_size=7, learning_rate=0.001, feature_size=2, item_size=4, max_feature_index=5)
+        dataset = FeatureAggregationSimilarityDataset(
+            x_item_indices=np.array([0, 1, 2]),
+            y_item_indices=np.array([3, 2, 1]),
+            x_item_features=np.array([[0, 1], [1, 2], [2, 4]]),
+            y_item_features=np.array([[5, 0], [0, 4], [3, 2]]),
+            scores=np.array([0.1, 0.2, 0.4]))
+
+        model.fit(dataset=dataset, batch_size=1, epoch_size=5, early_stopping_patience=2, test_size_rate=0.4)
+
+        similarities = model.calculate_similarity(
+            x_item_indices=dataset.x_item_indices,
+            y_item_indices=dataset.y_item_indices,
+            x_item_features=dataset.x_item_features,
+            y_item_features=dataset.y_item_features)
+        self.assertEqual(similarities.shape, (3, ))
+
+        embeddings = model.calculate_embeddings(item_features=np.array([[1, 2], [0, 0]]))
+        self.assertNotEqual(np.sum(embeddings[0]**2), 0)
+        self.assertEqual(np.sum(embeddings[1]**2), 0)  # feature index 0 must be masked index.
+
+
+if __name__ == '__main__':
+    unittest.main()
